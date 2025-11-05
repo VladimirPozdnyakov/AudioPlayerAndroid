2f29d74f68a180eeb5623f68e0df690c46e1c37d
Vladimir Pozdnyakov <pozdnyakov044@gmail.com>
2025-11-05 22:07:28 +0300

    0.6b

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.5b"
+        versionName = "0.6b"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
